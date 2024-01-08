9c35c841da2f70363a2c6abbc3b24a3d6488f885
최미래 <miraexhoi@gmail.com>
2024-01-08 12:22:50 +0900

    Update HelloLombok

--- src/main/java/com/mysite/springboot/HelloLombok.java
+++ src/main/java/com/mysite/springboot/HelloLombok.java
@@ -6,8 +6,15 @@ import lombok.Setter;
 @Setter
 @Getter
 public class HelloLombok {
-    private String hello;
-    private int lombok;
+    private final String hello;
+    private final int lombok;
+
+    // final : 불변
+
+    public HelloLombok(String hello, int lombok) {
+        this.hello = hello;
+        this.lombok = lombok;
+    }
 
     public static void main(String[] args) {
         HelloLombok helloLombok = new HelloLombok();

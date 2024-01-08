04e49f5004832ce689567a16d557be7e3d52a17e
최미래 <miraexhoi@gmail.com>
2024-01-08 11:34:43 +0900

    Update HelloController

--- src/main/java/com/mysite/springboot/HelloController.java
+++ src/main/java/com/mysite/springboot/HelloController.java
@@ -1,2 +1,14 @@
-package com.mysite.springboot;public class HelloController {
+package com.mysite.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
 }

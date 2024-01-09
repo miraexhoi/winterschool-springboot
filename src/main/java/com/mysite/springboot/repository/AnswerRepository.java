97e998c98fcfd339f7c72fea7a1a78e2e40d1b35
최미래 <miraexhoi@gmail.com>
2024-01-09 10:34:10 +0900

    Feat :: create AnswerRepository

---
+++ src/main/java/com/mysite/springboot/repository/AnswerRepository.java
@@ -0,0 +1,10 @@
+package com.mysite.springboot.repository;
+
+import com.mysite.springboot.entity.AnswerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<AnswerEntity, Integer> {
+    // JpaRepository는 CRUD 작업을 처리하는 메서드들을 내장
+    // JpaRepository<Question, Integer>는 Question 엔티티로 리포지터리를 생성한다는 의미
+}
+

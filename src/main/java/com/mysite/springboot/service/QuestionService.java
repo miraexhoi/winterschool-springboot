594e53f43a9a5d035bfc81ccbd915f92b87e96e2
최미래 <miraexhoi@gmail.com>
2024-01-11 11:38:01 +0900

    Add :: Add create Logic

--- src/main/java/com/mysite/springboot/service/QuestionService.java
+++ src/main/java/com/mysite/springboot/service/QuestionService.java
@@ -4,8 +4,14 @@ import com.mysite.springboot.exception.DataNotFoundException;
 import com.mysite.springboot.entity.QuestionEntity;
 import com.mysite.springboot.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,8 +19,11 @@ import java.util.Optional;
 @Service
 public class QuestionService {
     private final QuestionRepository questionRepository;
-    public List<QuestionEntity> getList() {
-        return this.questionRepository.findAll();
+    public Page<QuestionEntity> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 
     public QuestionEntity getQuestion(Integer id) {
@@ -23,5 +32,13 @@ public class QuestionService {
             return question.get();
         }
         else throw new DataNotFoundException("question not found");
+    }
+
+    public void create(String subject, String content) {
+        QuestionEntity q = new QuestionEntity();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(q);
     }
 }

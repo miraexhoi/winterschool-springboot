474de1bff60d315e54400350cffc51b8fadb4a2d
최미래 <miraexhoi@gmail.com>
2024-01-12 23:10:48 +0900

    Add :: Add QuestionEntity

--- src/main/java/com/mysite/springboot/entity/QuestionEntity.java
+++ src/main/java/com/mysite/springboot/entity/QuestionEntity.java
@@ -2,6 +2,7 @@ package com.mysite.springboot.entity;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.mysite.springboot.entity.AnswerEntity;
 import jakarta.persistence.*;
@@ -13,7 +14,6 @@ import lombok.Setter;
 @Setter
 @Entity
 public class QuestionEntity {
-
     // id : id 속성을 기본키로 지정, id 속성의 고유 번호들은 엔티티에서 각 데이터들을 구분하는 유효한 값 -> 중복 x
     // strategy = GenerationType.IDENTITY : 고유한 번호를 생성하는 방법을 지정하는 부분
     // GenerationType.IDENTITY : 해당 속성만 별도로 번호가 차례대로 늘어나도록 할 때 사용
@@ -35,4 +35,12 @@ public class QuestionEntity {
     // CascadeType.REMOVE : 질문을 삭제하면 그에 달린 답변들도 함께 삭제
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
     private List<AnswerEntity> answerList;
+
+    @ManyToOne
+    private SiteUser author;
+
+    private LocalDateTime modifyDate;
+
+    @ManyToMany
+    Set<SiteUser> voter;
 }

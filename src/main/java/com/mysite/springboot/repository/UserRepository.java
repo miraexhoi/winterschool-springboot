c3466dd131ed5258a638010b09f5fc3579a12b73
최미래 <miraexhoi@gmail.com>
2024-01-12 23:06:53 +0900

    Feat :: findByusername

--- src/main/java/com/mysite/springboot/repository/UserRepository.java
+++ src/main/java/com/mysite/springboot/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.mysite.springboot.repository;
 
+import java.util.Optional;
+
 import com.mysite.springboot.entity.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByusername(String username);
 }
